22bca8b0a3c8c004066736c6d9ced1bc85b61095
Emma Rose <emjrose@gmail.com>
2017-02-05 19:30:04 +0000

    Save sort order to local storage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
 import { createStore, applyMiddleware } from 'redux';
+import { loadState, saveState } from './helpers/localStorage';
 import bikesApp from './redux'
 import App from './App';
 import './index.css';
 
+const persistedState = loadState();
+const store = createStore(bikesApp, persistedState, applyMiddleware(thunk))
 
-const store = createStore(bikesApp, applyMiddleware(thunk))
+store.subscribe(throttle(() => {
+  saveState({
+    sortOrder: store.getState().sortOrder,
+  });
+}, 1000));
 
 ReactDOM.render(
   <Provider store={store}>
